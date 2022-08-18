1269b8ca48545130175f646d0c3f38134f27b7cd
chencan <2895100818@qq.com>
2022-08-18 13:57:58 +0800

    创建仓库

---
+++ PinkBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PinkBook
+//
+//  Created by beidixiaoxiong on 2022/8/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemPink
+    }
+
+
+}
+
